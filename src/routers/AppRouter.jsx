b2a0691c71af41d43f84349c5147fbac4653ea75
AmanCH3 <armanachaudhary53@gmail.com>
2025-05-14 15:39:40 +0545

    feat : Login Page UI

--- src/routers/AppRouter.jsx
+++ src/routers/AppRouter.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import  { BrowserRouter, Route , Routes} from 'react-router-dom'
+import LoginPage from '../pages/LoginPage'
 
 export default function AppRouter() {
   return (
     <BrowserRouter>
-    <Route>
-        <Route >
+    <Routes>
+        <Route path='/login' element = {<LoginPage/>}>
 
         </Route>
-    </Route>
+    </Routes>
     </BrowserRouter>
   )
 }
